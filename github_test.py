a3821328749dd60d0609d2ee03168bb26676206e
smartdev007 <kevadiajenish7@gmail.com>
2020-02-25 14:56:25 -0500

    Added mocking

--- github_test.py
+++ github_test.py
@@ -1,20 +1,41 @@
 """
     @author Jenish Kevadia
-
     Script imports methods from 'github.py' script and implements test cases for the github repositories
 """
 
 
 import unittest
+import json
 from github import get_repositories
+from unittest import mock
+
+def get_link(url):
+    """ Fetch URL """
+    if url == 'https://api.github.com/users/dev22/repos':
+        return fetch('repos.json')
+
+def fetch(path):
+    """ Open path file """
+    data = text()
+    with open(path, 'r') as f:
+        data.text = json.loads(f)
+    return data.text
+
+class text:
+    """ Declare text class for path file """
+    text = ""
+
 
 class TestGithub(unittest.TestCase):
     """ Test cases """
 
-    def test_get_repositories(self):
+    @mock.patch('requests.get')
+    def test_get_repositories(self, mockedReq):
         """ Test case to check the user's repositories and commits"""
+        mockedReq.side_effect = get_link
 
-        self.assertEqual(get_repositories('dev22'), ["Repo: dev, Number of commits: 2"])
+        repos = get_repositories('dev22')
+        self.assertEqual(repos, "Failed to fetch the repos")
 
 
 if __name__ == "__main__":
